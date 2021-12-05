ac58e3cf3edbb66411803b15b98c1a71b30a241b
a.werle <anatolij.werle@vitasystems.de>
2021-12-05 16:36:20 +0100

    delegate new_game from the API to the impl

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,4 +1,6 @@
 defmodule Hangman do
+  alias Hangman.Impl.Game
+
   @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
   @type game :: any
   @type tally :: %{
@@ -9,8 +11,7 @@ defmodule Hangman do
         }
 
   @spec new_game() :: game
-  def new_game() do
-  end
+  defdelegate new_game(), to: Game
 
   @spec make_move(game, String.t()) :: {game, tally}
   def make_move(_game, _guess) do
