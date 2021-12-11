0a1d1d701f21f50fc6fc6da83db15cbf255c54ef
a.werle <anatolij.werle@vitasystems.de>
2021-12-11 19:22:49 +0100

    add stick figure view helper

--- b1/lib/b1_web/views/hangman_view.ex
+++ b1/lib/b1_web/views/hangman_view.ex
@@ -1,3 +1,99 @@
 defmodule B1Web.HangmanView do
   use B1Web, :view
+
+  def figure_for(0) do
+    ~S"""
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========
+    """
+  end
+
+  def figure_for(1) do
+    ~S"""
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========
+    """
+  end
+
+  def figure_for(2) do
+    ~S"""
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========
+    """
+  end
+
+  def figure_for(3) do
+    ~S"""
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========
+    """
+  end
+
+  def figure_for(4) do
+    ~S"""
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========
+    """
+  end
+
+  def figure_for(5) do
+    ~S"""
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    """
+  end
+
+  def figure_for(6) do
+    ~S"""
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    """
+  end
+
+  def figure_for(7) do
+    ~S"""
+      +---+
+          |
+          |
+          |
+          |
+          |
+    =========
+    """
+  end
 end
