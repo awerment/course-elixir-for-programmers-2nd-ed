0e7605efdc4bfb4491b47860aa899fadfab93be6
a.werle <anatolij.werle@vitasystems.de>
2021-12-11 21:07:25 +0100

    add basic structure with live sub-components

--- b2/lib/b2_web/live/game.ex
+++ b2/lib/b2_web/live/game.ex
@@ -1,13 +1,23 @@
 defmodule B2Web.Live.Game do
   use B2Web, :live_view
 
+  alias __MODULE__
+
   def mount(_params, _session, socket) do
+    game = Hangman.new_game()
+    tally = Hangman.tally(game)
+
+    socket = assign(socket, game: game, tally: tally)
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    Hello from Hangman Live
+    <div class="game-holder">
+      <%= live_component(Game.Figure, tally: @tally, id: 1) %>
+      <%= live_component(Game.Alphabet, tally: @tally, id: 2) %>
+      <%= live_component(Game.WordSoFar, tally: @tally, id: 3) %>
+    </div>
     """
   end
 end
