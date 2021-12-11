dd86a4b7a75915c92954f25b0bf5c191da0fd7dd
a.werle <anatolij.werle@vitasystems.de>
2021-12-11 21:31:56 +0100

    add keyup event handler for making a guess by typing a letter

--- b2/lib/b2_web/live/game.ex
+++ b2/lib/b2_web/live/game.ex
@@ -13,11 +13,20 @@ defmodule B2Web.Live.Game do
 
   def render(assigns) do
     ~H"""
-    <div class="game-holder">
+    <div class="game-holder" phx-window-keyup="make_move">
       <%= live_component(Game.Figure, tally: @tally, id: 1) %>
       <%= live_component(Game.Alphabet, tally: @tally, id: 2) %>
       <%= live_component(Game.WordSoFar, tally: @tally, id: 3) %>
     </div>
     """
   end
+
+  @valid_keys ?a..?z |> Enum.to_list() |> List.to_string() |> String.codepoints()
+  def handle_event("make_move", %{"key" => key}, socket) when key in @valid_keys do
+    tally = Hangman.make_move(socket.assigns.game, key)
+    socket = assign(socket, tally: tally)
+    {:noreply, socket}
+  end
+
+  def handle_event("make_move", _, socket), do: {:noreply, socket}
 end
